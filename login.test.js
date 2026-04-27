9b6d25257a5627a464adf8b773dc45e66f3cd4b4
Erica Anfiloquio <ericafanfiloquio@gmail.com>
2026-04-27 11:39:53 -0300

    Adicionando o arquivo inicial com teste de login

---
+++ login.test.js
@@ -0,0 +1,34 @@
+import http from 'k6/http'
+import { sleep, check } from 'k6'
+
+export const options = {
+  iterations: 50, // esse número, é o tanto de vezes que o teste descrito abaixo vai ser executado
+  thresholds: {
+    http_req_duration: ['p(90)<1', 'max<10'],
+    http_req_failed: ['rate<0.01']
+  }
+};
+
+export default function () {
+  const url = 'http://localhost:3000/login'
+
+  const payload = JSON.stringify({ //esse JSON converte o texto de js para json - pra chamar k6, ele tem que estar em json
+    username: 'julio.lima',
+    senha: '123456',
+  });
+
+  const params = {
+    headers: { // isso é um objeto
+      'Content-Type': 'application/json',
+    },
+  }
+
+  const res = http.post(url, payload, params)
+
+  check(res, {
+    'Validar que o Status é 200': (r) => r.status === 200,
+    'Validar que o token é string': (r) => typeof(r.json().token) == 'string'
+  })
+
+  sleep(1)
+}
